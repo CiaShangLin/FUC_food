b8a57d859947acbef23dbe3df3cb93c34fa8097e
fcu-d0514534 <D0514534@gmail.com>
2019-01-13 21:47:58 +0800

    準備寫simpleMenu

--- app/src/main/java/com/shang/fcu_food/DetailShop/DetailShopActivity.kt
+++ app/src/main/java/com/shang/fcu_food/DetailShop/DetailShopActivity.kt
@@ -22,51 +22,51 @@ class DetailShopActivity : AppCompatActivity() {
         val POSITION = "POSITION"  //傳遞哪一個位置
     }
 
-    var position:Int=0
-    var tag:String=""
-    lateinit var adapter:Any
-    lateinit var options:Any
+    var position: Int = 0
+    var tag: String = ""
+    lateinit var adapter: Any
+    lateinit var options: Any
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_shop)
 
-        position=intent.extras.getInt(POSITION)
-        tag=intent.extras.getString(TAG)
-        Log.v("TAG","$position $tag")
+        position = intent.extras.getInt(POSITION)
+        tag = intent.extras.getString(TAG)
+        Log.v("TAG", "$position $tag")
+
         detailShopTb.setTitle(R.string.ShopInformation)
         detailShopTb.inflateMenu(R.menu.menu_detailshop)
 
-
         var query = FirebaseDatabase.getInstance().getReference().child(tag)
-        when(tag){
-            BreakfastShop.tag->
-                options=FirebaseRecyclerOptions.Builder<BreakfastShop>().setQuery(query,BreakfastShop::class.java).build()
-            DinnerShop.tag->
-                options=FirebaseRecyclerOptions.Builder<DinnerShop>().setQuery(query,DinnerShop::class.java).build()
-            SnackShop.tag->
-                options=FirebaseRecyclerOptions.Builder<SnackShop>().setQuery(query,SnackShop::class.java).build()
-            DrinkShop.tag->
-                options=FirebaseRecyclerOptions.Builder<DrinkShop>().setQuery(query,DrinkShop::class.java).build()
+        when (tag) {
+            BreakfastShop.tag ->
+                options = FirebaseRecyclerOptions.Builder<BreakfastShop>().setQuery(query, BreakfastShop::class.java).build()
+            DinnerShop.tag ->
+                options = FirebaseRecyclerOptions.Builder<DinnerShop>().setQuery(query, DinnerShop::class.java).build()
+            SnackShop.tag ->
+                options = FirebaseRecyclerOptions.Builder<SnackShop>().setQuery(query, SnackShop::class.java).build()
+            DrinkShop.tag ->
+                options = FirebaseRecyclerOptions.Builder<DrinkShop>().setQuery(query, DrinkShop::class.java).build()
         }
 
-        adapter = object : FirebaseRecyclerAdapter<Shop, DetailShopVH>(options as FirebaseRecyclerOptions<Shop>){
+        adapter = object : FirebaseRecyclerAdapter<Shop, DetailShopVH>(options as FirebaseRecyclerOptions<Shop>) {
             override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailShopVH {
-                var view = LayoutInflater.from(parent.context).inflate(R.layout.cardview_detailshop, parent, false)
+                var view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.cardview_detailshop, parent, false)
                 return DetailShopVH(view)
             }
+
             override fun onBindViewHolder(holder: DetailShopVH, position: Int, model: Shop) {
-                holder.bind(tag,holder,model)
+                holder.bind(tag, holder, model)
             }
         }
-        detailShopRecyc.layoutManager=LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        detailShopRecyc.adapter= adapter as FirebaseRecyclerAdapter<Shop, DetailShopVH>
+        detailShopRecyc.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        detailShopRecyc.adapter = adapter as FirebaseRecyclerAdapter<Shop, DetailShopVH>
         (adapter as FirebaseRecyclerAdapter<Shop, DetailShopVH>).startListening()
 
     }
-
-
 
 
 }
