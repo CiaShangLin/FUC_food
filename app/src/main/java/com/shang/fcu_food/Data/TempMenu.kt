7bbead6b80ec5b65621c6acbf3114d6572526da9
CiaShangLin <nick034po@yahoo.com.tw>
2019-01-18 18:04:38 +0800

    add menu dialog layout

---
+++ app/src/main/java/com/shang/fcu_food/Data/TempMenu.kt
@@ -0,0 +1,11 @@
+package com.shang.fcu_food.Data
+
+class TempMenu {
+    var name:String=""
+    var star:Double=0.0
+    var price:Int=0
+    var uid:String=""
+    var comment:String=""
+
+    constructor()
+}
