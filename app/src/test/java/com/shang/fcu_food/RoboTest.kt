34019b0ff208525818c3f743d477bbb2a76be659
fcu-d0514534 <D0514534@gmail.com>
2019-01-26 21:55:56 +0800

    新增RoboTest

---
+++ app/src/test/java/com/shang/fcu_food/RoboTest.kt
@@ -0,0 +1,19 @@
+package com.shang.fcu_food
+
+import com.shang.fcu_food.Main.MainActivity
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+
+@RunWith(RobolectricTestRunner::class)
+class RoboTest {
+
+    @Test
+    fun MainActivityTest(){
+
+    }
+
+}
