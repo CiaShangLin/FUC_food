2e8eda3f8e07a765c47e8e41cddb0f31e830cab6
CiaShangLin <nick034po@yahoo.com.tw>
2019-01-26 23:32:42 +0800

    Robo安裝成功

--- app/src/test/java/com/shang/fcu_food/RoboTest.kt
+++ app/src/test/java/com/shang/fcu_food/RoboTest.kt
@@ -1,19 +1,27 @@
 package com.shang.fcu_food
 
+import android.support.v7.widget.Toolbar
 import com.shang.fcu_food.Main.MainActivity
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
-import org.robolectric.annotation.Config
-
+import org.robolectric.Shadows
+import org.robolectric.shadow.api.Shadow
 
 @RunWith(RobolectricTestRunner::class)
 class RoboTest {
 
     @Test
     fun MainActivityTest(){
+        var mainActivity=Robolectric.setupActivity(MainActivity::class.java);
+        var toolbar=mainActivity.findViewById<Toolbar>(R.id.toolbar)
+        var shadow=Shadows.shadowOf(toolbar) as Toolbar
+
+
+        shadow.performClick()
+
+
 
     }
-
 }
