4eea178f8ee0f044d07de7deddedf3022cf65987
fcu-d0514534 <D0514534@gmail.com>
2019-01-28 21:41:30 +0800

    DetailActivty新增Menu按鈕

--- app/src/main/java/com/shang/fcu_food/DetailShop/DetailShopAdapter.kt
+++ app/src/main/java/com/shang/fcu_food/DetailShop/DetailShopAdapter.kt
@@ -1,4 +1,29 @@
 package com.shang.fcu_food.DetailShop
 
-class DetailShopAdapter {
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.shang.fcu_food.Data.*
+import com.shang.fcu_food.R
+
+class DetailShopAdapter(var shop_tag: String, options: FirebaseRecyclerOptions<Any>) :
+    FirebaseRecyclerAdapter<Any, DetailShopVH>(options) {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, position: Int): DetailShopVH {
+        var view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.cardview_detailshop, parent, false)
+        return DetailShopVH(view)
+    }
+
+    override fun onBindViewHolder(holder: DetailShopVH, position: Int, model: Any) {
+        Log.d("TAG",(model is BreakfastShop).toString())
+        Log.d("TAG",(model is DinnerShop).toString())
+        Log.d("TAG",(model is SnackShop).toString())
+        Log.d("TAG",(model is DrinkShop).toString())
+    }
+
+
 }
