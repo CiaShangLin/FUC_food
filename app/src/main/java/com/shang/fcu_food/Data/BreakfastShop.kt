68f301697149c6627a876312ef81c8fad99b7c1c
fcu-d0514534 <D0514534@gmail.com>
2019-02-04 21:46:03 +0800

    測試Shop

--- app/src/main/java/com/shang/fcu_food/Data/BreakfastShop.kt
+++ app/src/main/java/com/shang/fcu_food/Data/BreakfastShop.kt
@@ -1,24 +1,36 @@
 package com.shang.fcu_food.Data
 
 import com.firebase.ui.database.FirebaseRecyclerOptions
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import com.shang.fcu_food.R
 
 class BreakfastShop : Shop() {
     companion object {
-        val tag:String="breakfast"
+        val tag: String = "breakfast"
+        var allBreakfastShop: MutableList<BreakfastShop> = mutableListOf<BreakfastShop>()
     }
 
-    override var errorDrawable:Int= R.drawable.ic_shop
+    override var errorDrawable: Int = R.drawable.ic_shop
 
     override fun getQuery(): DatabaseReference? {
-        return FirebaseDatabase.getInstance().getReference().child(tag)
+        var query = FirebaseDatabase.getInstance().getReference().child(tag)
+        query.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {}
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                allBreakfastShop.clear()
+                for(data in snapshot.children){
+                    var breakfastShop=data.getValue(BreakfastShop::class.java)
+                    allBreakfastShop.add(breakfastShop!!)
+                }
+            }
+        })
+        return query
     }
 
     override fun getOption(): FirebaseRecyclerOptions<Shop> {
-        var options=FirebaseRecyclerOptions.Builder<BreakfastShop>()
-            .setQuery(getQuery()!!,BreakfastShop::class.java).build()
+        var options = FirebaseRecyclerOptions.Builder<BreakfastShop>()
+            .setQuery(getQuery()!!, BreakfastShop::class.java).build()
         return options as FirebaseRecyclerOptions<Shop>
     }
 }
