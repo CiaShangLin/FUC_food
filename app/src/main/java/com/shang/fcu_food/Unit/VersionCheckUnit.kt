45bf536d79bcfe4f65ff3999f1a379d7209e3621
fcu-d0514534 <D0514534@gmail.com>
2019-02-21 23:48:42 +0800

    新增版本檢查Unit

--- app/src/main/java/com/shang/fcu_food/Unit/VersionCheckUnit.kt
+++ app/src/main/java/com/shang/fcu_food/Unit/VersionCheckUnit.kt
@@ -1,6 +1,8 @@
 package com.shang.fcu_food.Unit
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Handler
 import android.os.Message
 import com.google.firebase.database.DataSnapshot
@@ -17,42 +19,46 @@ class VersionCheckUnit {
         val VERSION_CHECK_OLD = 150
         val VERSION_ERROR = 404
 
-        fun checkVersion(context: Context, handler: Handler){
+        fun checkVersion(context: Context, handler: Handler) {
             var verison = FirebaseDatabase.getInstance().getReference("version")
                 .addListenerForSingleValueEvent(object : ValueEventListener {
                     override fun onCancelled(p0: DatabaseError) {
-                        showErrorAlert(context,p0.message)
+                        showErrorAlert(context, p0.message)
                     }
 
                     override fun onDataChange(p0: DataSnapshot) {
-                        var versionCode = context.packageManager.getPackageInfo(context.packageName, 0).versionCode.toString()
+                        var versionCode =
+                            context.packageManager.getPackageInfo(context.packageName, 0).versionCode.toString()
                         var version = p0.getValue(Version::class.java)
-                        if(version?.release.equals(versionCode)){
+                        if (version?.release.equals(versionCode)) {
                             handler.handleMessage(Message().apply {
-                                this.what= VERSION_CHECK_NEW
+                                this.what = VERSION_CHECK_NEW
                             })
-                        }else{
-                            showUpdateAlert(context,version?.information.toString())
+                        } else {
+                            showUpdateAlert(context, version?.information.toString())
                         }
                     }
                 })
         }
 
-        fun showUpdateAlert(context: Context,message: String){
+        private fun showUpdateAlert(context: Context, message: String) {
             context.alert {
-                this.title="有新版本囉"
-                this.message="請問要前往更新嗎?\n"+message
-                this.positiveButton("前往",onClicked = {
-
+                this.title = "請問要前往更新嗎"
+                this.message = message
+                this.positiveButton("前往", onClicked = {
+                    context.startActivity(Intent().apply {
+                        this.action = Intent.ACTION_VIEW
+                        this.setData(Uri.parse("https://play.google.com/store/apps/details?id=com.shang.fcu_food"))
+                    })
                 })
-                this.isCancelable=false
+                this.isCancelable = false
             }.show()
         }
 
-        fun showErrorAlert(context: Context,message: String){
+        private fun showErrorAlert(context: Context, message: String) {
             context.alert {
-                this.title="有新版本囉"
-                this.message="發生了未知錯誤\n"+message
+                this.title = "ERROR"
+                this.message = "發生了未知錯誤\n" + message
             }.show()
         }
     }
